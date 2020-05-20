2153ad3b8260dd67ceccaaeb2216773fed637931
binod <binod@BINOD-PC>
2020-05-21 02:27:52 +0530

    service added

---
+++ hello-world-spring-boot/src/main/java/com/javadevelopersguide/springboot/SpringBootApplicationLuncher.java
@@ -0,0 +1,18 @@
+package com.javadevelopersguide.springboot;
+
+// TODO Auto-generated method stub
+import org.springframework.boot.SpringApplication;
+
+/**
+		 * This Luncher for the spring boot application.
+		 * 
+		 * @author Binod
+		 *
+		 */
+		public class SpringBootApplicationLuncher {
+		public static void main(String[] args) {
+		SpringApplication.run(HelloWorldController.class, args);
+		}
+		}
+
+
